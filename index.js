e4ad0addcf0fbdc6248e34f0236f2b6178ffe734
azoladyantyi <aizydyantyi@gmail.com>
2017-09-21 12:04:23 +0200

    My first commit

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const form = require('body-parser');
 // const session = require('express-session');
 const bodyParser = require('body-parser');
 const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/registration";
-const Models = require("./models");
-const models = Models(mongoURL);
-
+const models = require("./models");
+var regNumbers = models();
+// const models = Models(mongoURL);
+//
 var app = express();
-
-//set up the engine
+//
+var regNumbers = models();
+var mongoose = require('mongoose')
+// //set up the engine
 app.engine("handlebars", exphbs({
     defaultLayout: "main",
     extname: "handlebars"
@@ -22,33 +25,43 @@ app.use(form.urlencoded({
 }));
 app.set("view engine", "handlebars")
 
+mongoose.connect('mongodb://localhost/registration');
+ var db = mongoose.connection;
+ //throw err
+
+ db.on('error', console.error.bind(console, 'connection error:'));
+
 app.get("/", function(req, res) {
+  var regNumber = req.body.name;
     res.render("home");
 });
-
+//
 var regList = [];
 app.post("/reg_numbers", function(req, res) {
-    var regNumber = req.body.name;
-    var reg = "";
-    reg = regNumber;
-    // regList.push(regNumber);
-    res.render("home", {
-        displayReg: reg
-    })
-    regList.push(regNumber);
-});
-app.get('/allReg', function(req, res) {
-    res.render('home', {
-        registrationNum: regList
+  var regNumber = req.body.name;
 
-    });
+  regList.push(regNumber)
+  models.create({name: regNumber}, function(err, result){
+      if (err) {return (err)}
+  });
+
+  res.render('home',{
+      displayReg: regList
+
+    })
+    // var reg = "";
+    // reg = regNumber;
+    // res.render("home", {
+    //     displayReg: reg
+    // })
 });
 
 //start the server
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
 
-    var host = server.address().address;
-    var port = server.address().port;
+ var host = server.address().address;
+ var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+ console.log('Registration web app listening at http://%s:%s', host, port);
+
 });
--- models.js
+++ models.js
@@ -1,15 +1,16 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const mongoURL = process.env.MONGO_DB_URL || "mongodb://localhost/registration";
+const MongoURL =process.env.MONGO_DB_URL || "mongodb://localhost/registration";
+mongoose.connect(mongoURL,{
+  regAvailable: true
+});
+console.log("Connecting to MongoURL : " + MongoURL);
+const regNumbers = mongoose.Schema({
 
-module.exports = function (mongoURL) {
-  mongoose.connect(mongoURL);
-  const regNumberSchema = mongoose.Schema({
-    name: String,
-    counter: Number
-  })
-  const Name = mongoose.model("Name", regNumberSchema)
+  name: String
+ });
 
-  return {
-    Name
-  }
+var regNumber = mongoose.model("regNumber", regNumbers);
 
-}
+module.exports = regNumber;
